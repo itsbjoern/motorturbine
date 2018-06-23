77d9897b5adb8c34920b97320cc6b6ca246fa2d8
Björn Friedrichs <bjoern@friedrichs1.de>
2018-06-23 22:11:09 +0200

    Change package configs

--- motorturbine/__init__.py
+++ motorturbine/__init__.py
@@ -1,2 +1,5 @@
 from . import errors, connection, fields
 from .document import BaseDocument
+
+__version__ = '0.1.0'
+name = 'motorturbine'
--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='motorturbine',
-      version='0.1',
+      version='0.1.0',
       description='A ORM package for asyncio and motor',
-      url='http://github.com/BFriedrichs/aiomotorengine',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/BFriedrichs/motorturbine',
       author='Björn Friedrichs',
       author_email='bjoern@friedrichs1.de',
       license='MIT',
       packages=find_packages(),
       zip_safe=False,
       include_package_data=True,
-      package_data={'motorturbine': ['motorturbine']})
+      package_data={'motorturbine': ['motorturbine']},
+      classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+      ])
